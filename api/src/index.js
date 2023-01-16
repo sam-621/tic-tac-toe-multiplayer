98d068d5fbd936d273a90e4cf93c2d0dd82e0aa7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-01-15 23:36:23 -0700

    refactor:  add module option

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
 const app = express()
-const http = require('http')
 const server = http.createServer(app)
-const { Server } = require('socket.io')
 const io = new Server(server)
 
 app.get('/', (req, res) => {
