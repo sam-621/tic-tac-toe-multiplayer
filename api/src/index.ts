3725e3d5b85d0545be9ba9336ab9dbfee2adb189
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-01-15 23:49:39 -0700

    feat:  add ts support
    add nodemon

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,10 +6,6 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
 io.on('connection', (socket) => {
   socket.on('game-room', (input) => {
     socket.join(input.code)
