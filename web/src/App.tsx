d7233fe4bf733e91abc55d2ce26071a120bdeed5
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-03-23 11:13:47 -0700

    feat(tailwind):  add colors and typography

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
+import HomePage from './pages';
+
 function App() {
-  return <h1>Hola</h1>;
+  return <HomePage />;
 }
 
 export default App;
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/ui/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/ui/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
+    fontFamily: {
+      display: ['"Outfit"']
+    },
     extend: {
-      colors: {}
+      colors: {
+        'light-blue': '#31C3BD',
+        'light-blue-hover': '#65E9E4',
+        'light-yellow': '#F2B137',
+        'light-yellow-hover': '#FFC860',
+        'dark-navy': '#1A2A33',
+        'semi-dark-navy': '#1F3641',
+        silver: '#A8BFC9',
+        'silver-hover': '#DBE8ED'
+      }
     }
   },
   plugins: []
