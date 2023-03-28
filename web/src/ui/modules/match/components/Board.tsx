575c47fa6c5b8d98b8f4d036feb71bde64d94dd1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-03-28 09:43:25 -0700

    refactor(ui):  board component

--- web/src/ui/modules/match/components/Board.tsx
+++ web/src/ui/modules/match/components/Board.tsx
@@ -6,60 +6,14 @@ export const Board = () => {
 
   return (
     <main className="grid grid-cols-3 gap-5 place-items-center">
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
-      <button
-        type="button"
-        className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
-      >
-        <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
-      </button>
+      {new Array(9).map(() => (
+        <button
+          type="button"
+          className="bg-semi-dark-navy shadow-inset-card  w-24 h-24 sm:w-36 sm:h-36 rounded-lg flex justify-center items-center"
+        >
+          <CrossIcon width={isDesktop ? 64 : 40} height={isDesktop ? 64 : 40} />
+        </button>
+      ))}
     </main>
   );
 };
