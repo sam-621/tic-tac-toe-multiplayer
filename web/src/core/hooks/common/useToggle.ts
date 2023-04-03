30eb5e5d8c6ea2c86cc380ff2bf4984f9137b9c0
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-04-02 23:22:57 -0600

    feat(core):  add useToggle

---
+++ web/src/core/hooks/common/useToggle.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export const useToggle = (initialValue = false) => {
+  const [state, setState] = useState(initialValue);
+
+  const toggle = () => {
+    setState(!state);
+  };
+
+  return {
+    state,
+    toggle,
+    setState
+  };
+};
