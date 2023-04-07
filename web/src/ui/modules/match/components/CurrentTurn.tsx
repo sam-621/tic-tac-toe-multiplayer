58dec76621ff9775ed7dd71dc2a12037d781cc0d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-04-07 03:22:02 -0700

    feat(match):  add CurrentTurn to CurrentTurn component

--- web/src/ui/modules/match/components/CurrentTurn.tsx
+++ web/src/ui/modules/match/components/CurrentTurn.tsx
@@ -1,9 +1,17 @@
-import { CrossIcon } from '@/shared/common';
+import { Player } from '@/interfaces/Game';
+import { CrossIcon, NoughtIcon } from '@/shared/common';
+import { useAppSelector } from '@/store/rootState';
 
 export const CurrentTurn = () => {
+  const { currentTurn } = useAppSelector(state => state.match);
+
   return (
     <div className="flex items-center gap-2 p-4 shadow-inset-card-tiny bg-semi-dark-navy rounded-md">
-      <CrossIcon className="fill-silver" width={16} height={16} />
+      {currentTurn === Player.CROSSES ? (
+        <CrossIcon className="fill-silver" width={16} height={16} />
+      ) : (
+        <NoughtIcon className="fill-silver" width={16} height={16} />
+      )}
       <span className="text-silver tracking-wider text-sm font-bold">TURN</span>
     </div>
   );
