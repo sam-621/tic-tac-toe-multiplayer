adfbeccdf06e5496a1039b5b354b454ba4f75b00
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-04-14 20:16:41 -0700

    feat(api):  add build commands for production

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,8 @@ import { matchStarted, MATCH_STARTED } from './events/game-started'
 import { matchMove, MATCH_MOVE_CHANNEL } from './events/game-move'
 import { MATCH_FINISHED, matchFinished } from './events/match-finished'
 
+const PORT = process.env.PORT ?? 4000
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
@@ -22,6 +24,6 @@ io.on('connection', (socket) => {
   socket.on(MATCH_FINISHED, (input) => matchFinished(input, io))
 })
 
-server.listen(4000, () => {
-  console.log('listening on *:4000')
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`)
 })
